6f84c466a4eeba8fe06d04c20185b9c1466838ac
Mathias Schustereder <schustereder@student.tugraz.at>
2026-04-06 16:29:01 +0200

    Add basic placeholder version of PECC UI

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,33 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('pecc-interface');
+  private http = inject(HttpClient);
+  
+  protected readonly title = signal('PECC Interface');
+  protected readonly responseMessage = signal('');
+
+  private apiUrl = 'https://mschustereder-pecc.hf.space/chat';
+
+  sendRequest() {
+    const payload = { message: "Austria" };
+    
+    this.http.post<any>(this.apiUrl, payload).subscribe({
+      next: (res) => {
+        console.log('Success:', res);
+        this.responseMessage.set(res.reply);
+      },
+      error: (err) => {
+        console.error('Error:', err);
+        this.responseMessage.set('Connection failed.');
+      }
+    });
+  }
 }
